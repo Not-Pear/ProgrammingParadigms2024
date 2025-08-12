00a38f54346beac747bc1e3cc5e30dae7954c20a
Shane Norden <snorden@uark.edu>
2025-08-12 18:38:39 -0500

    Add files via upload

---
+++ Polymorphism/Model.java
@@ -0,0 +1,275 @@
+import java.util.ArrayList;
+import java.io.*;
+import java.lang.*;
+import java.util.*;
+import java.awt.Point;
+
+// Sorts map items by their y-values
+
+class ItemYComparator implements Comparator<MapItem>
+{
+	// Returns a negative value if a.y > b.y
+	// Returns zero if a.y == b.y
+	// Returns a positive value if a.y < b.y
+	public int compare(MapItem a, MapItem b) {
+		return a.y - b.y;
+  }
+}
+class MapItem
+{
+	protected int x;
+	protected int y;
+	protected int type;
+	//int scale;
+
+	protected MapItem(int x, int y, int type)
+	{
+		this.x = x;
+		this.y = y;
+		this.type = type;
+	}
+
+	Json marshal()
+	{
+		//System.out.println("MapItem marshal called");
+		Json ob = Json.newObject();
+		ob.add("type", this.type);
+		ob.add("x", this.x);
+		ob.add("y", this.y);
+		return ob;
+	}
+
+	public static MapItem unmarshal(Json ob) {
+		//System.out.println("MapItem unmarshal called");
+		return newItem((int)ob.getLong("x"), (int)ob.getLong("y"), (int)ob.getLong("type"));
+	}
+	Point pos(int time)
+	{
+		return new Point(this.x, this.y);
+	}
+	public static MapItem newItem(int x, int y, int type) {
+		if (type == 3 || type == 9)
+			return new Jumper(x, y, type);
+		else if (type == 4 || type == 7)
+			return new Horizontal(x, y, type);
+		else if (type == 2)
+			return new Scaler(x, y, type);
+		else
+			return new MapItem(x, y, type);
+	}
+	public double scale(int time)
+	{
+		return 1.0;
+	}
+
+}
+
+class Model
+{
+	
+
+	ArrayList<MapItem> items;
+	int turtle_x;
+	int turtle_y;
+	int dest_x;
+	int dest_y;
+	static int speed = 10;
+	public int current_item;
+
+	
+	// ItemYComparator compareY;
+
+
+	Model()
+	{
+		this.turtle_x = 100;
+		this.turtle_y = 100;
+		this.dest_x = 150;
+		this.dest_y = 100;
+		this.items = new ArrayList();
+
+	}
+
+	public void update()
+	{
+		if(this.turtle_x < this.dest_x)
+            this.turtle_x += Math.min(speed, this.dest_x - this.turtle_x);
+		else if(this.turtle_x > this.dest_x)
+            this.turtle_x -= Math.max(speed, this.dest_x - this.turtle_x);
+		if(this.turtle_y < this.dest_y)
+            this.turtle_y += Math.min(speed, this.dest_y - this.turtle_y);
+		else if(this.turtle_y > this.dest_y)
+            this.turtle_y -= Math.max(speed, this.dest_y - this.turtle_y);
+	}
+
+    public void reset()
+    {
+        this.turtle_x = 200;
+        this.turtle_y = 200;
+        this.dest_x = this.turtle_x;
+        this.dest_y = this.turtle_y;
+    }
+
+	public void setDestination(int x, int y)
+	{
+		this.dest_x = x;
+		this.dest_y = y;
+	}
+	public void addMapItem(int x, int y)
+	{
+		MapItem i = MapItem.newItem(x, y, this.current_item);
+		this.items.add(i);
+	}
+	public void change_item()
+	{
+		if(this.current_item == 9){
+			this.current_item = 0;
+		}
+		else{
+			this.current_item = this.current_item + 1;
+		}
+		
+	}
+	public int getNumValues(){
+		return this.items.size();
+	}
+
+
+	public void deleteMapItem(int x, int y)
+	{
+		if(this.getNumValues() == 0){
+			return;
+		}
+		MapItem min = this.items.get(0);
+		int tempValX = min.x;
+		int tempValY = min.y;
+		int minDistance = (tempValX  - x) * (tempValX  - x) + (tempValY - y) * (tempValY - y);
+		int minNum = 0;
+		for(int i = 0; i < this.getNumValues(); i++)
+		{
+			MapItem temp = this.items.get(i);
+			int xVal = temp.x;
+			int yVal = temp.y;
+
+			int distance = (xVal - x) * (xVal - x) + (yVal - y) * (yVal - y);
+			if(distance < minDistance)
+			{
+				minDistance = distance;
+				minNum = i;
+			}
+
+
+		}
+		this.items.remove(minNum);
+	}
+	public void sortValues(){
+
+		this.items.sort(new ItemYComparator());
+	}
+	public Json marshal(View view)
+	{
+		Json map = Json.newObject();
+		Json list_of_map_items = Json.newList();
+		map.add("items", list_of_map_items);
+		for (MapItem item : this.items)
+		{
+			list_of_map_items.add(item.marshal());
+			map.add("horScroll", view.horScroll);
+			map.add("vertScroll", view.vertScroll);
+		}
+		//System.out.println("model marshal called");
+		return map;
+	}
+
+	public void unmarshal(Json ob, View view)
+	{
+
+		this.items = new ArrayList<MapItem>();
+		Json tempList = ob.get("items");
+		for(int i = 0; i < tempList.size(); i++){
+			MapItem temp = MapItem.unmarshal(tempList.get(i));
+			this.items.add(temp);
+		}
+		
+		
+		view.horScroll = (int)ob.getLong("horScroll");
+		view.vertScroll = (int)ob.getLong("vertScroll");
+		//System.out.println("model unmarshal called");
+
+
+	}
+
+}
+class Jumper extends MapItem
+{
+	int x;
+	int y;
+	int type;
+
+	Jumper(int x, int y, int type)
+	{
+		super(x, y, type);
+		this.x = x;
+		this.y = y;
+		this.type = type;
+		//this.super(this.x, this.y, this.type);
+		
+		//super();
+	}
+
+	Point pos(int time)
+	{
+		return new Point(this.x, this.y - (int)Math.max(0., 50 * Math.sin(((double)time) * 2 * Math.PI / 30)));
+	}
+	public double scale(double time)
+	{
+		return 1.0;
+	}
+}
+class Horizontal extends MapItem
+{
+	int x;
+	int y;
+	int type;
+	//int scale;
+
+	Horizontal(int x, int y, int type)
+	{
+		super(x, y, type);
+		this.x = x;
+		this.y = y;
+		this.type = type;
+	}
+
+	Point pos(int time)
+	{
+		return new Point(this.x - (int)Math.max(0., 50 * Math.sin(((double)time) * 2 * Math.PI / 30)), this.y);
+	}
+	public double scale(int time)
+	{
+		return 1.0;
+	}
+}
+class Scaler extends MapItem
+{
+	int x;
+	int y;
+	int type;
+
+	Scaler(int x, int y, int type)
+	{
+		super(x, y, type);
+		this.x = x;
+		this.y = y;
+		this.type = type;
+	}
+
+	Point pos(int time)
+	{
+		return new Point(this.x, this.y);
+	}
+	public double scale(int time)
+	{
+		return 1.5 + Math.sin(((double)time) / 5);
+	}
+}
---
+++ Polymorphism/View.java
@@ -0,0 +1,109 @@
+import javax.swing.JPanel;
+import java.awt.Graphics;
+import java.awt.image.BufferedImage;
+import javax.imageio.ImageIO;
+import java.io.File;
+import javax.swing.JButton;
+import java.awt.Color;
+import java.awt.Point;
+import java.lang.Math;
+
+class View extends JPanel
+{
+	JButton save_button;
+	JButton load_button;
+	BufferedImage[] item_images;
+	Model model;
+	public static int horScroll;
+	public static int vertScroll;
+	public static int time;
+	View(Model m)
+	{
+		this.model = m;
+		this.save_button = new JButton("Save Button");
+		this.add(save_button);
+		save_button.setFocusable(false);
+
+		
+		this.load_button = new JButton("Load Button");
+		this.add(load_button);
+		load_button.setFocusable(false);
+		// Load the images
+		this.item_images = new BufferedImage[Main.MapItemTypes.length];
+		for (int i = 0; i < Main.MapItemTypes.length; i++) {
+			BufferedImage image = null;
+			try
+			{
+				image = ImageIO.read(new File("images/" + Main.MapItemTypes[i] + ".png"));
+			} catch(Exception e) {
+				e.printStackTrace(System.err);
+				System.exit(1);
+			}
+			this.item_images[i] = image;
+		}		
+
+		/*try
+		{
+			this.turtle_image = ImageIO.read(new File("images/turtle.png"));
+		} catch(Exception e) {
+			e.printStackTrace(System.err);
+			System.exit(1);
+		}*/
+	}
+
+	public void paintComponent(Graphics g)
+	{
+		// Clear the background
+		time++;
+		int width = this.getWidth();
+		int height = this.getHeight();
+
+		//System.out.println("Scale Cord Hor -  " + horScroll);
+		//System.out.println("Scale Cord Vert -  " + vertScroll);
+		//System.out.println(" horScroll 1 -  " + horScroll);
+		//System.out.println(" vertScroll 1 -  " + vertScroll);
+
+		g.setColor(new Color(64, 255, 128));
+		g.fillRect(0, 0, this.getWidth(), this.getHeight());
+
+		// Draw all the item image
+
+		for (int i = 0; i < model.items.size(); i++) {
+
+			MapItem item = model.items.get(i);
+			Point p = item.pos(this.time);
+			double s = item.scale(this.time);
+			BufferedImage image = this.item_images[item.type];
+			int w = image.getWidth();
+			int h = image.getHeight();
+			int sw = (int)(w * s);
+			int sh = (int)(h * s);
+			int dx1 =  p.x - sw / 2 - this.horScroll;
+			int dy1 = p.y - sh - this.vertScroll;
+			//vertScroll =  (item.x - w / 2) - vertScroll;
+			//horScroll = (item.y - h) - horScroll;
+			//System.out.println(" View Cord Hor -  " + horScroll);
+			//System.out.println(" View Cord Vert -  " + vertScroll);
+
+			// Draw the image with the bottom center at (x, y)
+			// g.drawImage(image, (p.x - w / 2) - this.horScroll, (p.y - h) - this.vertScroll, null);
+			g.drawImage(image, dx1, dy1, dx1 + sw, dy1 + sh, 0, 0, w, h, null);
+			//	drawImage(Image img, int dx1, int dy1, int dx2, int dy2, int sx1, int sy1, int sx2, int sy2, Color bgcolor, ImageObserver observer)
+		}
+
+		g.setColor(new Color(160, 32, 240));
+		g.fillRect(0, 0, 200, 200);
+		// Draw the image so that its bottom center is at (x,y)
+		/*int w = this.turtle_image.getWidth();
+		int h = this.turtle_image.getHeight();
+		g.drawImage(this.turtle_image, model.turtle_x - w / 2, model.turtle_y - h, null);*/
+		g.drawImage(this.item_images[this.model.current_item], 0, 0, null);
+	}
+	
+	void removeButton()
+	{
+
+		this.repaint();
+	}
+
+}
